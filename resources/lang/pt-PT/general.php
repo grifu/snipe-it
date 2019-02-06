f9c36ec7ac4c3a70186f0583b2d4560889f4b809
Grifu <luis.grifu@gmail.com>
2019-02-06 22:56:45 +0000

    Language update

--- resources/lang/pt-PT/general.php
+++ resources/lang/pt-PT/general.php
@@ -208,6 +208,7 @@
     'update'                => 'Atualizar',
     'upload_filetypes_help'      => 'Allowed filetypes are png, gif, jpg, jpeg, doc, docx, pdf, xls, txt, lic, zip, and rar. Max upload size allowed is :size.',
     'uploaded'              => 'Carregado',
+    'responsible'			=> 'Responsável',
     'user'					=> 'Utilizador',
     'accepted'			    => 'aceite',
     'declined'			    => 'recusado',
