8957adda8d9552f106b00cdac6fad34b82a715c2
Grifu <luis.grifu@gmail.com>
2019-02-06 22:59:53 +0000

    update

--- resources/views/partials/forms/edit/user-select.blade.php
+++ resources/views/partials/forms/edit/user-select.blade.php
@@ -1,16 +1,40 @@
-<div id="assigned_user" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}"{!!  (isset($style)) ? ' style="'.e($style).'"' : ''  !!}>
-
+<div id="assigned_user" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}"{!!  (isset($style)) ? ' style="'.e($style).'"' : ''  !!}>  
     {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
 
     <div class="col-md-7{{  ((isset($required)) && ($required=='true')) ? ' required' : '' }}">
-        <select class="js-data-ajax" data-endpoint="users" data-placeholder="{{ trans('general.select_user') }}" name="{{ $fieldname }}" style="width: 100%" id="assigned_user_select">
-            @if ($user_id = Input::old($fieldname, (isset($item)) ? $item->{$fieldname} : ''))
+
+    
+
+        <select class="js-data-ajax" data-endpoint="users" data-placeholder="{{ trans('general.select_user') }}" name="{{ $fieldname }}" style="width: 100%" id="{{ (isset($user_id)) ? $user_id : '1' }}" >
+
+    
+            
+               <!-- GRIFU:  this value is passed through requestou.blade.php-->
+                @if (!empty($user_id))
                 <option value="{{ $user_id }}" selected="selected">
-                    {{ (\App\Models\User::find($user_id)) ? \App\Models\User::find($user_id)->present()->fullName : '' }}
-                </option>
-            @else
-                <option value="">{{ trans('general.select_user') }}</option>
-            @endif
+                    <!--$users = DB::table('users')->select('name', 'email as user_email')->get();-->
+                        {{ (\App\Models\User::find($user_id)) ? \App\Models\User::find($user_id)->present()->fullName : '' }}
+                    </option>
+                @endif
+                
+                <!-- GRIFU:  Temporary - This should be implemented to filter users within just a group-->
+                @if (!empty($group_id))
+                <option label="responsible" value="{{ $group_id }}" selected="selected">
+                        {{ (\App\Models\Group::where('id', 1)->get()) ? \App\Models\User::find($group_id)->present()->fullName : '' }}
+                    </option>
+
+                    @else
+                        @if ($user_id = Input::old($fieldname, (isset($item)) ? $item->{$fieldname} : ''))
+                        <option value="{{ $user_id }}" selected="selected">
+                            {{ (\App\Models\User::find($user_id)) ? \App\Models\User::find($user_id)->present()->fullName : '' }}
+                        </option>
+                    @else
+                        <option value="">{{ trans('general.select_user') }}</option>
+                    @endif
+                @endif
+
+                
+
         </select>
     </div>
 
